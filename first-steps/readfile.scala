2c5ea0cd8b9a5e41cb93d7fe6c2a5ff2a5edcdcc
iamrp <pathak.ravi1989@gmail.com>
2019-08-15 02:15:43 +0530

    padding file lines

--- first-steps/readfile.scala
+++ first-steps/readfile.scala
@@ -1,13 +1,20 @@
 import scala.io.Source
 
 if(args.length >0 ){
-  var max=-1;
-  for( line <- Source.fromFile(args(0)).getLines())
+  var max = -1
+
+  val lines = Source.fromFile(args(0)).getLines().toList
+
+  for( line <- lines)
     if (max > line.length) max = line.length 
   
-  val size = (max + "").length
+  val maxsize = (max + "").length
 
-
+  for ( line <- lines){
+    val padsize = maxsize - line.length.toString.length
+    val pad = " " * padsize
+    println(s"${line.length}${pad}|${line}")
+  }
 }
 else
   Console.err.println("Please provide filename")
